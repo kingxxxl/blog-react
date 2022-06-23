c2935677138a5bf67e651d7998fa27f53fdd426c
kingxxxl <103191276+kingxxxl@users.noreply.github.com>
2022-06-23 15:50:28 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import AddBlog from "./pages/AddBlog";
+import NotFound from "./pages/NotFound";
+import Blog from "./pages/Blog";
+
 
 function App() {
     return (
         <>
+            <BrowserRouter>
+                <Navbar/>
+                <div className='container'>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/about' element={<About/>}/>
+                        <Route path='/contact' element={<Contact/>}/>
+                        <Route path='/add-blog' element={<AddBlog/>}/>
+                        <Route path='/blog/:id' element={<Blog/>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </>
     );
 }
